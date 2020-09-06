396ea38fe5e57832291c9e6746be542917e37ddc
taylorkraft <taylorkraft16@gmail.com>
2020-09-06 16:45:45 -0600

    remove compont did mount

--- src/App.js
+++ src/App.js
@@ -2,12 +2,6 @@ import React from 'react';
 
 class App extends React.Component {
 
-  componentDidMount() {
-    fetch('http://localhost:3000/api/v1/portfolios')
-    .then(resp => resp.json())
-    .then(respObj => console.log(respObj))
-  }
-
   render() {
     return (
       <div className="App">
