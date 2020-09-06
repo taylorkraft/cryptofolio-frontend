384c1a8005367f8e7f53b1d46a99ef6004230df1
taylorkraft <taylorkraft16@gmail.com>
2020-09-06 13:22:31 -0600

    refacot App (function to class component)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      App
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        App
+      </div>
+    );
+  }
 }
 
 export default App;
