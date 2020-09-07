be566ca6be0e384ef3400e4473864a8aca9c805a
taylorkraft <taylorkraft16@gmail.com>
2020-09-07 09:12:05 -0600

    setup initial form

--- src/components/PortfolioForm.js
+++ src/components/PortfolioForm.js
@@ -2,10 +2,31 @@ import React from 'react'
 
 class PortfolioForm extends React.Component {
 
+  state = {
+    name:'',
+    balance: ''
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = () => {
+    
+  }
+
   render() {
     return (
       <div>
-        PortfolioForm
+        <form onSubmit={this.handleSubmit}>
+          <label>Portfolio Name: </label>
+          <input type="text" placeholder="Name" value={this.state.name} name="name" onChange={this.handleChange}/><br/>
+          <label>Portfolio Balance: </label>
+          <input type="text" placeholder="Balance" value={this.state.balance} name="balance" onChange={this.handleChange}/><br/>
+          <input type="submit"/><br/><br/>
+        </form>
       </div>
     )
   }
