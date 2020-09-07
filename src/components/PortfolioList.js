18d552d48e63cb7955e98748d0f0c76c883ca1ba
taylorkraft <taylorkraft16@gmail.com>
2020-09-07 13:09:10 -0600

    portfolio list update

--- src/components/PortfolioList.js
+++ src/components/PortfolioList.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import Portfolio from './Portfolio'
 
 const PortfolioList = (props) => {
 
   //we are iterating over all our portfolios that are coming in as props from our PortfoliosContainer
   // which is coming in through our store from mapStateToProps
+  // for each portfolio we create a Portfolio component
+  // that Portfolio component must accept a portfolio as props
   return (
     <div>
       {props.portfolios.map(portfolio => 
-      <li key={portfolio.id}> {portfolio.name} - {portfolio.balance}</li>)}
+      <li key={portfolio.id}><Portfolio portofolio={portfolio}/></li>)}
     </div>
   )
 }
