fab7ded6a9f42c35ed3fde901463a9bdd4042495
taylorkraft <taylorkraft16@gmail.com>
2020-09-08 09:36:02 -0600

    setup for NavBar/Css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { connect } from 'react-redux'
+import './App.css'
 import PortfoliosContainer from './containers/PortfoliosContainer'
 
 class App extends React.Component {
