7a3b48b912fa1bcb81958e4f80528dbf2e08e7c8
taylorkraft <taylorkraft16@gmail.com>
2020-09-07 19:26:42 -0600

    trade form fields/submit setup

--- src/components/TradeForm.js
+++ src/components/TradeForm.js
@@ -3,16 +3,28 @@ import { connect } from 'react-redux'
 
 class TradeForm extends React.Component {
 
+  state = {
+    amount: '',
+    trade_type: ''
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
 
   render() {
     return (
       <div>
         <form>
-          <label>Trade Type: </label>
-          <select>
+          <label>Trade Amount: </label>
+          <input type="text" name="amount" value={this.props.amount} onChange={this.handlChange}/>
+          <select name="trade_type" value={this.state.trade_type} onChange={this.handlChange}>
             <option>buy</option>
             <option>sell</option>
           </select>
+          <input type="submit"/><br/><br/>
         </form>
       </div>
     )
